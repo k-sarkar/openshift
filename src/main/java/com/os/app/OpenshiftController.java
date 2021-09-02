12d18981830aab598d63705833ec0fad6c09b7cc
koushik <sarkar_koushik@yahoo.com>
2021-09-02 11:35:26 -0500

    adding controller

---
+++ src/main/java/com/os/app/OpenshiftController.java
@@ -0,0 +1,13 @@
+package com.os.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenshiftController {
+
+	@GetMapping("/user")
+	public String getUser() {
+		return "Koushik Sarkar";
+	}
+}
