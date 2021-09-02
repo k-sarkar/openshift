a3cf2098ff4d8fab743dab63656e758ec443fe82
koushik <sarkar_koushik@yahoo.com>
2021-09-02 16:08:20 -0500

    adding environment message

--- src/main/java/com/os/app/OpenshiftController.java
+++ src/main/java/com/os/app/OpenshiftController.java
@@ -1,13 +1,23 @@
 package com.os.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenshiftController {
+	
+	@Value("$(message:default message)")
+	private String message;
 
 	@GetMapping("/user")
 	public String getUser() {
 		return "Koushik Sarkar";
 	}
+	
+	@GetMapping("/message")
+	public String getGetmessage() {
+		return message;
+	}
+	
 }
